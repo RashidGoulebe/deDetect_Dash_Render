7a7446742259e3e563d372bcdb008adde7b9d94f
Rashid Goulebe <r.goulebe@e-dynamics.de>
2024-03-14 14:11:42 +0100

    First input of CSV Data

--- edDetect_viz.py
+++ edDetect_viz.py
@@ -2,7 +2,7 @@ from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+df = pd.read_csv('eddetect_output_advanced_clean_02.csv', sep=';')
 
 app = Dash(__name__)
 
@@ -10,18 +10,22 @@ app = Dash(__name__)
 server = app.server
 app.layout = html.Div([
     html.H1(children='Title of Dash App', style={'textAlign': 'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    dcc.Dropdown(df.city.unique(), 'Aachen', id='city'),
+    dcc.Dropdown(['pres', 'prcp'], 'pres', id='metric'),
     dcc.Graph(id='graph-content')
 ])
 
 
+
 @callback(
     Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
+    Input('city', 'value'),
+    Input('metric', 'value')
 )
-def update_graph(value):
-    dff = df[df.country == value]
-    return px.line(dff, x='year', y='pop')
+def update_graph(city,metric):
+
+    dff = df[df.city == city]
+    return px.line(dff, x='ds', y=metric)
 
 
 if __name__ == '__main__':
