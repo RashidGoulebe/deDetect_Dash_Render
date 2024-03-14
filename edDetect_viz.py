e2bb31473567bb3449b79d90d077611700048354
Rashid Goulebe <r.goulebe@e-dynamics.de>
2024-03-14 11:37:00 +0100

    Update

--- edDetect_viz.py
+++ edDetect_viz.py
@@ -1,77 +1,26 @@
-import plotly.graph_objects as go
-import numpy as np
-#test
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import pandas as pd
 
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
 
-x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-x_rev = x[::-1]
+app = Dash(__name__)
 
-# Line 1
-y1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-y1_upper = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-y1_lower = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-print(x+x_rev)
-y1_lower = y1_lower[::-1]
-print(y1_upper+y1_lower)
+# Rander.com
+server = app.server
+app.layout = html.Div([
+    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    dcc.Graph(id='graph-content')
+])
 
-# Line 2
-y2 = [5, 2.5, 5, 7.5, 5, 2.5, 7.5, 4.5, 5.5, 5]
-y2_upper = [5.5, 3, 5.5, 8, 6, 3, 8, 5, 6, 5.5]
-y2_lower = [4.5, 2, 4.4, 7, 4, 2, 7, 4, 5,4]
-y2_lower = y2_lower[::-1]
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+def update_graph(value):
+    dff = df[df.country==value]
+    return px.line(dff, x='year', y='pop')
 
-# Line 3
-y3 = [10, 8, 6, 4, 2, 0, 2, 4, 2, 0]
-y3_upper = [11, 9, 7, 5, 3, 1, 3, 5, 3, 1]
-y3_lower = [9, 7, 5, 3, 1, -.5, 1, 3, 1, -1]
-y3_lower = y3_lower[::-1]
-
-
-fig = go.Figure()
-
-fig.add_trace(go.Scatter(
-    x=x+x_rev,
-    y=y1_upper+y1_lower,
-    fill='toself',
-    fillcolor='rgba(0,100,80,0.2)',
-    line_color='rgba(255,255,255,0)',
-    showlegend=False,
-    name='Fair',
-))
-
-fig.add_trace(go.Scatter(
-    x=x+x_rev,
-    y=y2_upper+y2_lower,
-    fill='toself',
-    fillcolor='rgba(0,176,246,0.2)',
-    line_color='rgba(255,255,255,0)',
-    name='Premium',
-    showlegend=False,
-))
-fig.add_trace(go.Scatter(
-    x=x+x_rev,
-    y=y3_upper+y3_lower,
-    fill='toself',
-    fillcolor='rgba(231,107,243,0.2)',
-    line_color='rgba(255,255,255,0)',
-    showlegend=False,
-    name='Ideal',
-))
-fig.add_trace(go.Scatter(
-    x=x, y=y1,
-    line_color='rgb(0,100,80)',
-    name='Fair',
-))
-fig.add_trace(go.Scatter(
-    x=x, y=y2,
-    line_color='rgb(0,176,246)',
-    name='Premium',
-))
-fig.add_trace(go.Scatter(
-    x=x, y=y3,
-    line_color='rgb(231,107,243)',
-    name='Ideal',
-))
-
-fig.update_traces(mode='lines')
-fig.show()
+if __name__ == '__main__':
+    app.run(debug=True)
